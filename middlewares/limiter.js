2184698acfcc122b55a5a707ebcb52878121b323
Gremwiz1 <gremwiz@yandex.ru>
2021-08-18 02:20:56 +0300

    Fix mistake in limiter middlewares

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -4,4 +4,4 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
-module.exports(limiter);
+module.exports = limiter;
