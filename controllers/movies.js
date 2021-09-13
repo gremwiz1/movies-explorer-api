975b7a0d93f0aecb164aba2322bbba669033496c
Gremwiz1 <gremwiz@yandex.ru>
2021-09-13 15:11:41 +0300

    change getMovies

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,7 +5,8 @@ const ForbiddenError = require("../errors/forbidden-err");
 const ANSWER = require("../utils/answers");
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  const owner = req.user._id;
+  Movie.find({ owner })
     .then((movies) => res.status(200).send(movies))
     .catch(next);
 };
